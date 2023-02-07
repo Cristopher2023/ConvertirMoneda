67eb5f980018fec8b7b0470814a9fc1f86360458
Cristopher2023 <bromanaguilar@gmail.com>
2023-02-06 23:39:00 -0600

    primer avanze

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+
+import com.conversor.CoversorMoneda;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        CoversorMoneda con1 = new CoversorMoneda();
+
+        con1.setCantidad(Float.parseFloat(JOptionPane.showInputDialog("Ingresa cantidad:")));
+
+        System.out.println(con1.toString());
+
     }
 }
